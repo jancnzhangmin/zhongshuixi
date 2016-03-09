197d1d9ffa32092e9ea5cd2bb68ffafafaeb7eca
jancnzhangmin <jancnzhangmin@hotmail.com>
2016-03-09 08:41:11 +0800

    init

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,33 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+
+//= require bootstrap.js
+//= require jquery.parallax.js
+//= require modernizr-2.6.2.min.js
+//= require revolution-slider/js/jquery.themepunch.revolution.min.js
+//= require jquery.nivo.slider.pack.js
+//= require jquery.prettyPhoto.js
+//= require superfish.js
+//= require tweetMachine.js
+//= require tytabs.js
+//= require jquery.sticky.js
+//= require jflickrfeed.js
+//= require imagesloaded.pkgd.min.js
+//= require waypoints.min.js
+//= require spectrum.js
+//= require switcher.js
+//= require jquery.gmap.min.js
+//= require custom.js
