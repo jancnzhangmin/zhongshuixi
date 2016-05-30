e11d6e2d4c7cd01880556dbd20e482d5424c368a
jancnzhangmin <jancnzhangmin@hotmail.com>
2016-05-31 06:58:43 +0800

    2016-5-31

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,9 +4,8 @@ class NewsController < ApplicationController
 
 
     @cla = Cla.find_by(keyword:"news")
-    @news = @cla.contents.order("updated_at DESC").paginate :page => params[:page], per_page:8
-    @cla2 = Cla.find_by(keyword:"product")
-    @products = @cla2.contents
+    @news = @cla.contents.order("updated_at DESC")
+
 
 
 
@@ -20,10 +19,8 @@ class NewsController < ApplicationController
   def show
 
 
-    @news=Content.find(params[:id])
-    @newss = Content.all
-    @cla2 = Cla.find_by(keyword:"product")
-    @products = @cla2.contents
+    @new=Content.find(params[:id])
+
 
 
 
